0e1d0fa63a2ce775cfc90a531c2b5d7fe40513a0
muratbyk05 <95565340+muratbyk05@users.noreply.github.com>
2022-02-24 11:13:10 +0300

    Add files via upload

---
+++ takmlar/code.js
@@ -0,0 +1,6 @@
+let ulkeler=[""]
+function takımGoster(ulke){
+    let secilenUlke=ulke.toUppercase();
+    document.getElementById("takımAdı").innerHTML="seçilen ülke "+secilenUlke;
+    document.getElementsByClassName("takımBilgleri")[0].innerHTML="asdadasdad"
+}
